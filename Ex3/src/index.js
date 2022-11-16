68e7ebfdefcb2c612f8b2a46d087702f8c0d647d
Aluno <aluno@etec.sp.gov.br>
2022-11-16 20:32:55 -0300

    Atividade 16/11

--- Ex3/src/index.js
+++ Ex3/src/index.js
@@ -1,18 +1,40 @@
 /*
-Monte um vetor de 6 lugares que irá sortear 
-6 numeros entre 1 a 6 aleatoriamente, com as seguintes condições
-1. Os 6 números não podem ser iguais.
-2. Eles devem ser apresentados em ordem crescente.
-3. Os 3 primeiros numeros não devem estar na casa 0 ao 20 e do 21 ao 45
-4. Os 3 ultimos devem estar na casa dos 46 ao 55 e o ultimo 56 até 60.
+1) Monte um vetor de 6 lugares, que adicione um numero da sorte para cada posição como palpite para um jogo na mega-sena.
+
+2) Os números devem seguir as seguintes regras:
+	a. 1º, 2º e 3º devem estar entre 01 até 24.
+	b. 4º, 5º e 6º devem estar entre 25 e 60. 
+	c. Os números não podem ser repetir.
+	d. Os numeros precisam ser gerados com uso de um laço dde repetição.
+	e. Ao apresentar os numeros vem ser colocados de for crescente no console.
+
+3) O código deve utilizar a sboas práticas de funções para ser executado.
 */
 
-const numeros = new Array("n1", "n2", "n3", "n4", "n5", "n6")
-function numeros(){
-    console.log(numeros.sort(function(a, b, c, d, e, f){return a-21, b, c, d, e, f}))
-    for(let i=0;i<=indice;i++){
-        console.log(i == 60)
-    }
- }
+function NumeroAleatorio() {
+    
+    let aleatorios = new Array();
+    for (let i = 0; i < 6; i++){
+        let aleatorio = Math.floor(Math.random() * 60);
 
-numeros (60, 7);
+        for (let i = 0; i < aleatorios.length; i++){
+            while (aleatorio == aleatorios[i]) {
+                aleatorio = Math.floor(Math.random()* 60);
+                i = 0;
+            }
+        }
+
+        if (aleatorio >= 1 && aleatorio <= 24 && i <= 2){
+            aleatorio.push(aleatorio);
+        }
+        else if (aleatorio >= 25 && aleatorio <= 60 && i >= 3){
+            aleatorio.push(aleatorio);
+        }
+        else{
+            i--
+        }
+    }
+    return aleatorios.sort();
+}
+
+console.log(NumeroAleatorio());
